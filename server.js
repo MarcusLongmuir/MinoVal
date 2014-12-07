250ef24acfe6a631105d8cdda0e5ee18f4503f7c
bestan <bestan93@gmail.com>
2014-12-07 16:44:06 +0000

    Updated minoval usage in the test server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var errorHandler = require('errorhandler');
 
 var MinoDB = require('minodb');
 var db_address = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/minodb';
-var minodb = new MinoDB({
+var mino = new MinoDB({
     api: true,
     ui: true,
     db_address: db_address
@@ -21,17 +21,29 @@ server.set('view engine', 'jade')
 server.use(errorHandler());
 server.use(bodyParser());
 
-server.use('/mino/', minodb.server())
+server.use('/mino/', mino.server())
 
 var MinoVal = require('./minoval');
+var minoval = new MinoVal({
+	user: "my_app"
+});
 
-var minoval = new MinoVal();
-minodb.add_plugin(minoval);
+mino.add_plugin(minoval);
 
 server.get('/*', function(req, res) {
     res.send("MinoVal 404");
 })
 
-http.createServer(server).listen(server.get('port'), function() {
-    console.log('Server started on port ' + server.get('port'));
+
+mino.create_user({
+	"username": "my_app",
+	"email": "marcus+test@minocloud.com",
+	"password": "my_password"
+}, function(err, res){
+
+	logger.log("CREATED USER");
+
+	http.createServer(server).listen(server.get('port'), function() {
+	    console.log('Server started on port ' + server.get('port'));
+	});
 });
